6f3983eabd967981d38c00e2bc432595d7aaa389
Mukesh Yadav <ydvmukesh72@gmail.com>
2023-10-06 00:04:07 +0530

    import  component also creat

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,10 +1,13 @@
+import Test from "./test"
 
 
 function App() {
   
-
   return (
+    <>
    <h2>Hello | Vite</h2>
+   <Test/>
+   </>
   )
 }
 
---
+++ 01_vitereact/src/test.jsx
@@ -0,0 +1,6 @@
+function Test (){
+    return(
+        <h2>Componet of test using vite react</h2>
+    )
+}
+export default Test
